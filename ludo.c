7b2bf41297a46d264d9e1e999771eceda35ab3b1
Hugo De Albuquerque Fonseca <hugoalbuquerque0@gmail.com>
2016-05-13 00:47:01 -0300

    movimento ok

--- ludo.c
+++ ludo.c
@@ -15,6 +15,7 @@
 #define ANSI_COLOR_RESET   "\x1b[0m"
 #define pcor(X) pf((X)==0?(ANSI_COLOR_RESET):((X)==1?(ANSI_COLOR_RED):((X)==2?(ANSI_COLOR_GREEN):((X)==3?(ANSI_COLOR_BLUE):(ANSI_COLOR_YELLOW)))))
 #define pclr(X) pf((X)==0?(ANSI_COLOR_RESET):((X)==1?(ANSI_COLOR_GREEN):((X)==2?(ANSI_COLOR_RED):((X)==3?(ANSI_COLOR_BLUE):(ANSI_COLOR_YELLOW)))))
+#define ABS(X) ((X)>0?((X)):(-(X)))
 
 int iNIPOS_X[BNUM],
     iNIPOS_Y[BNUM];
@@ -40,14 +41,13 @@ int main(void)
 {
     unsigned i, j;
     unsigned px = 1, py = 0;
-    char c;
-    iNIPOS_X[0] = 3;
+    iNIPOS_X[0] = 12;
     iNIPOS_Y[0] = 3;
-    iNIPOS_X[1] = 12;
+    iNIPOS_X[1] = 3;
     iNIPOS_Y[1] = 3;
-    iNIPOS_X[2] = 12;
+    iNIPOS_X[2] = 3;
     iNIPOS_Y[2] = 12;
-    iNIPOS_X[3] = 3;
+    iNIPOS_X[3] = 12;
     iNIPOS_Y[3] = 12;
     for(i=0;i<PNUM;i++)
         for(j=0;j<BNUM;j++)
@@ -60,12 +60,25 @@ int main(void)
         }
     printmap();
 
-    for(i=0;i<80;i++)
+    unsigned a;
+    pf("Select Player:\n");
+    pclr(1);pf("Player 1\n");pclr(0);
+    pclr(2);pf("Player 2\n");pclr(0);
+    pclr(3);pf("Player 3\n");pclr(0);
+    pclr(4);pf("Player 4\n");pclr(0);
+    pf(">>");
+    scanf("%u", &a);
+    a--;
+    bmove(a,0);
+    printmap();
+    do
     {
-        bmove(2,0);
+        pf("Select the number of steps your pawn will take. (0 to exit)\n>>");
+        scanf("%u", &i);
+        for(j=0;j<i;j++)
+            bmove(a,0);
         printmap();
-        printf("(% 2u,% 2u)\n",p[2].b[0].x,p[2].b[0].y);
-    }
+    }while(i);
     return 0;
 }
 
@@ -143,26 +156,67 @@ void bmove(int plr, int blk)
         xi,
         yi,
         ei;
-    xend[0] = 14;
+    xend[0] = 2;
     yend[0] = 7;
-    xend[1] = 7;
+    xend[1] = 9;
     yend[1] = 2;
-    xend[2] = 2;
+    xend[2] = 14;
     yend[2] = 9;
-    xend[3] = 9;
+    xend[3] = 7;
     yend[3] = 14;
 
     xi = p[plr].b[blk].x;
     yi = p[plr].b[blk].y;
     ei = p[plr].b[blk].end;
-
-    /* If block is passing by the origin for the second time - UNDER CONSTRUCTION*/
-    /*if(xi == xend[plr] && yi == yend[plr] && ei != 0)
+    if(((8-xi) == 0 && ABS(8-yi) == 1) ||(ABS(8-xi) == 1 && (8-yi) == 0))
     {
-        p[plr].b[blk].x = 9;
-        p[plr].b[blk].y = 6;
+        printf("ERROR: attempting to call bmove(%u,%u) with the pawn already at the center!!\n",plr,blk);
         return;
-    }*/
+    }
+
+    /* If block is inside the end lane */
+    if(ei == 2)
+    {
+        if((8-xi) > 0)
+            p[plr].b[blk].x++;
+        if((8-xi) < 0)
+            p[plr].b[blk].x--;
+        if((8-yi) > 0)
+            p[plr].b[blk].y++;
+        if((8-yi) < 0)
+            p[plr].b[blk].y--;
+        xi = p[plr].b[blk].x;
+        yi = p[plr].b[blk].y;
+        printf("Only %d spaces left!!!\n",ABS(16-yi-xi)-1);
+        if(ABS(16-yi-xi) == 1)
+            printf("Player %u reached the center with one pawn!\n",plr);
+        return;
+    }
+    /* If block is passing by the origin for the second time - UNDER CONSTRUCTION*/
+    if(xi == xend[plr] && yi == yend[plr] && ei == 1)
+    {
+        p[plr].b[blk].end++;
+        switch(plr)
+        {
+            case 0:
+                p[plr].b[blk].x = 2;
+                p[plr].b[blk].y = 8;
+                break;
+            case 1:
+                p[plr].b[blk].x = 8;
+                p[plr].b[blk].y = 2;
+                break;
+            case 2:
+                p[plr].b[blk].x = 14;
+                p[plr].b[blk].y = 8;
+                break;
+            case 3:
+                p[plr].b[blk].x = 8;
+                p[plr].b[blk].y = 14;
+                break;
+        }
+        return;
+    }
     /* If block is passing by the origin for the first time*/
     if(xi == xend[plr] && yi == yend[plr] && ei == 0)
         p[plr].b[blk].end++;
@@ -177,91 +231,91 @@ void bmove(int plr, int blk)
             }
     /* If block is in a corner */
     /* UR Corner */
-    if(xi == 7 && yi == 1)
+    if(xi == 9 && yi == 1)
     {
         p[plr].b[blk].y++;
         return;
     }
     /* UL Corner */
-    if((xi == 9 || xi == 8) && yi == 1)
+    if((xi == 7 || xi == 8) && yi == 1)
     {
-        p[plr].b[blk].x--;
+        p[plr].b[blk].x++;
         return;
     }
     /* RU Corner */
-    if(xi == 15 && yi == 7)
+    if(xi == 1 && yi == 7)
     {
-        p[plr].b[blk].x--;
+        p[plr].b[blk].x++;
         return;
     }
     /* RD Corner */
-    if(xi == 15 && (yi == 9 || yi == 8))
+    if(xi == 1 && (yi == 9 || yi == 8))
     {
         p[plr].b[blk].y--;
         return;
     }
     /* DL Corner */
-    if(xi == 9 && yi == 15)
+    if(xi == 7 && yi == 15)
     {
         p[plr].b[blk].y--;
         return;
     }
     /* DR Corner */
-    if((xi == 7 || xi == 8) && yi == 15)
+    if((xi == 9 || xi == 8) && yi == 15)
     {
-        p[plr].b[blk].x++;
+        p[plr].b[blk].x--;
         return;
     }
     /* LD Corner */
-    if(xi == 1 && yi == 9)
+    if(xi == 15 && yi == 9)
     {
-        p[plr].b[blk].x++;
+        p[plr].b[blk].x--;
         return;
     }
     /* LU Corner */
-    if(xi == 1 && (yi == 7 || yi == 8))
+    if(xi == 15 && (yi == 7 || yi == 8))
     {
         p[plr].b[blk].y++;
         return;
     }
-    
-    /* Corner 1 (10,7) -> (9,6)*/
-    if(xi == 10 && yi == 7)
+
+    /* Corner 1 (6,7) -> (7,6)*/
+    if(xi == 6 && yi == 7)
     {
-        p[plr].b[blk].x = 9;
+        p[plr].b[blk].x = 7;
         p[plr].b[blk].y = 6;
         return;
     }
-    /* Corner 2 (7,6) -> (6,7)*/
-    if(xi == 7 && yi == 6)
+    /* Corner 2 (9,6) -> (10,7)*/
+    if(xi == 9 && yi == 6)
     {
-        p[plr].b[blk].x = 6;
+        p[plr].b[blk].x = 10;
         p[plr].b[blk].y = 7;
         return;
     }
-    /* Corner 3 (6,9) -> (7,10)*/
-    if(xi == 6 && yi == 9)
+    /* Corner 3 (10,9) -> (9,10)*/
+    if(xi == 10 && yi == 9)
     {
-        p[plr].b[blk].x = 7;
+        p[plr].b[blk].x = 9;
         p[plr].b[blk].y = 10;
         return;
     }
-    /* Corner 4 (9,10) -> (10,9)*/
-    if(xi == 9 && yi == 10)
+    /* Corner 4 (7,10) -> (6,9)*/
+    if(xi == 7 && yi == 10)
     {
-        p[plr].b[blk].x = 10;
+        p[plr].b[blk].x = 6;
         p[plr].b[blk].y = 9;
         return;
     }
     /* General movement rules*/
-    /* Upwards (9,y) -> (9,y-1)*/
-    if(xi == 9)
+    /* Upwards (7,y) -> (7,y-1)*/
+    if(xi == 7)
     {
         p[plr].b[blk].y--;
         return;
     }
-    /* Downwards (7,y) -> (7,y+1)*/
-    if(xi == 7)
+    /* Downwards (9,y) -> (9,y+1)*/
+    if(xi == 9)
     {
         p[plr].b[blk].y++;
         return;
@@ -269,13 +323,13 @@ void bmove(int plr, int blk)
     /* Rightwards (x,7) -> (x-1,7)*/
     if(yi == 7)
     {
-        p[plr].b[blk].x--;
+        p[plr].b[blk].x++;
         return;
     }
     /* Leftwards (x,9) -> (x+1,9)*/
     if(yi == 9)
     {
-        p[plr].b[blk].x++;
+        p[plr].b[blk].x--;
         return;
     }
 }
